f4bed0219b0cff1bb805026c4d2306227f408db0
Rithik Gupta <rithikg2700@gmail.com>
2023-11-26 20:45:16 +0530

    project1

---
+++ advJava1/src/main/java/beans/UserBean.java
@@ -0,0 +1,74 @@
+package beans;
+
+import java.sql.SQLException;
+
+import dao.UserDaoImpl;
+import pojos.User;
+
+public class UserBean {
+//props 
+	private UserDaoImpl userDao;// dependency
+	private User userDetails;// result
+	// clnt info
+	private String email;
+	private String pass;
+
+	// def ctor
+	public UserBean() throws SQLException {
+		// create user dao instance
+		userDao = new UserDaoImpl();
+		System.out.println("user bean created...");
+	}
+
+	// generate getter n setter
+	public UserDaoImpl getUserDao() {
+		return userDao;
+	}
+
+	public void setUserDao(UserDaoImpl userDao) {
+		this.userDao = userDao;
+	}
+
+	public User getUserDetails() {
+		return userDetails;
+	}
+
+	public void setUserDetails(User userDetails) {
+		this.userDetails = userDetails;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	// add a B.L method to authenticate user
+	public String authenticateUser() throws SQLException {
+		System.out.println("in JB B.L auth user " + email + " " + pass);
+		// JB ---> DAo's method
+		userDetails = userDao.authenticateUser(email, pass);
+		if (userDetails == null) // => invalid login
+			return "login"; // JB rets dyn navigational outcome (i.e telling JSP where to take the clnt
+							// next)
+		//=> login successful
+		if(userDetails.getRole().equals("admin"))
+			return "admin_page";
+		//=> voter 
+		if(userDetails.isVotingStatus())
+			return "logout";
+		//=> voter not yet voter
+		return "candidate_list";
+	}
+
+}
